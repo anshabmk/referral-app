68e68bc67c431c7fc6a3c9bead45f9b533276abd
Anshab M K <anshabmk@live.com>
2023-04-02 06:19:25 +0530

    Add Material UI

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 const Home = () => {
   return (
     <div>
       <h1>React Home page here.</h1>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 };
--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 document.addEventListener("turbo:load", () => {
   const root = createRoot(
     document.body.appendChild(document.createElement("div"))
